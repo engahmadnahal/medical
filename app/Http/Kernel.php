70665b1f2eb91e8fd28358b6b75d98085061a5b9
Ahmad Nahal <ahmadGhssanNahal@gmail.com>
2022-03-07 12:48:37 +0200

    fix error session

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,9 +21,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
